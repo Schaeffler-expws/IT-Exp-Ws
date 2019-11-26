50599146a4dbbc0c7eae15939756eef3d8548853
Lars Brehm <lars.brehm@hm.edu>
2019-11-26 13:58:37 +0100

    A10 Update

--- A10_Intro_in_Python.py
+++ A10_Intro_in_Python.py
@@ -1,61 +1,71 @@
+# Welcome to our Schaeffler Training Center! This is a Python program file. It is not really beautiful and structured, but good enough to see (!) and feel Python.
 
-# Welcome! This is a Python program file.
-
-# The content is adapted from a course from https://raw.githubusercontent.com/
-# raspberrypilearning/python-intro/master/code/intro.py
-# But, it's more fancy to use the LEDs. Actually, ignore the next 2 lines.
+# The main idea is adapted from a course from https://raw.githubusercontent.com/raspberrypilearning/python-intro/master/code/intro.py
+# The next two lines you should already know.
 
 from sense_hat import SenseHat
 sense = SenseHat()
 
 # The lines that start with a hash "#" are comments
-# They are for you to read and are ignored by Python
-
-# When you see 'GO!', save and run the file to see the output
+# When you see 'Start!', save and run the file to see the output
 # When you see a line starting with "#" follow the instructions
 # Some lines are python code with a "#" in front
 # This means they're commented out - remove the "#" to uncomment
 # Do one challenge at a time, save and run after each one!
 
-# If it is too boring to see all former output of your work,
-# set a "#" in front of the former output task.
+# If it is too boring to see all former output of your work, 
+# set a "#" in front of the former output (only output!) task. 
 
 # 1a. This is the print statement on the screen
 
-print("Hello LearningLab")
+print("Hello IT-Experience Workshop")
 
-# 1b. Show Message on our LEDs
+# 1b. Show Message on our LEDs (we had that already).
+# Of course, you are forced to change the text as well.
 
-sense.show_message("Hello Learning Lab")
+sense.show_message("Hello Kanban")
 
-# GO!
+# Start!
 
-# 2. This is a variable. A variable has a value that can change.
 
-message = "Success"
+# === NEXT ===
+# 2. Now, we come to a variable. A variable has a value that can change. 
 
-# Add a line below to print this variable on our LEDs. Try to add
-# , text_colour=[255,0,255]  after the variable, but still in the bracket.
+message = "Kanban Empty"
 
-# GO!
+# Add a line below to print this variable on our LEDs. Try to add 
+# ", text_colour=[255,0,255]"  after the variable, but still in the bracket.
+# Have a look in the documentation for senseHAT to change the background, colour, etc.
 
-# 3. The variable above is called a string
+# Start!
+
+
+# === NEXT ===
+# 3. The variable above is called a string (i.e. consists of characters)
 # You can use single or double quotes (but must close them)
 # You can ask Python what type a variable is. Try uncommenting the next line:
+
 # print(type(message))
+
 # Could you write the same code for the LEDs?
 
-# GO!
+# Start!
 
+
+# === NEXT ===
 # 4. Another type of variable is an integer (a whole number)
+
 a = 123
 b = 654
 c = a + b
 
-# Try printing the value of c below to see the answer - of course on the
+# Try printing the value of c below to see the answer - of course on the 
 # LEDs with a yellow background!
-# GO!
+# Start!
 
+
+
+# === NEXT ===
 # 5. Variables keep their value until you change it
 
 a = 100
@@ -64,86 +74,100 @@ a = 100
 c = 50
 # print(c)  # think - should this be 50 or 777?
 
-age = 10 + a - c
-# print(age)  # think - what should this be now?
+weight = 10 + a - c
+# print(weight)  # think - what should this be now?
 
-# GO!
+# Start!
 
+
+
+# === NEXT ===
 # 6. You can also use '+' to add together two strings
 
 greeting = 'Hi '
-name = ''  # enter your name in this string
+name = 'SBOT'  
 
 message = greeting + name
 # sense.show_message(message)
 
-# GO!
+# Start!
 
-# 7. We can convert numbers to strings like this. I'm sorry,
+
+# === NEXT ===
+# 7. We can convert numbers to strings like this. I'm sorry, 
 # you can't mix numbers and strings.
 
-# print(name + ' is ' + str(age) + ' years old')
+# print(name + ''s load' + ' is ' + str(weight) + ' kg heavy')
 
-# GO!
+# Start!
 
+
+# === NEXT ===
 # 8. Another variable type is called a boolean
 # This means either True or False
 
-raspberry_pi_is_fun = True
-raspberry_pi_is_expensive = False
+experience_workshop_is_fun = True
+experience_workshop_is_boring = False
 
 # We can also compare two variables using ==
 
-bobs_age = 15
-# your_age =  # fill in your age
+sbot1_weight = 15
+sbot2_weight = 20
 
-# print(your_age == bobs_age)  # this prints either True or False.
+# print(sbot1_weight == sbot2_weight)  # this prints either True or False. 
 # Again, we can't use our LED for this "complex" calculation in the brackets.
 
-# GO!
+# Start!
 
-# 9. We can ask questions before printing with an if statement
 
-money = 500
-phone_cost = 240
-tablet_cost = 200
 
-total_cost = phone_cost + tablet_cost
-can_afford_both = money > total_cost
+# === NEXT ===
+# # 9. We can ask questions before printing with an if statement
 
-if can_afford_both:# is a true value
-   message = "You have enough money for both"
+max_weight = 500
+skrew_weight = 240
+nut_weight = 200
+
+total_weight = skrew_weight + nuts_weight
+can_afford_both = max_weight > total_weight
+
+if can_afford_both:   # is a true value
+    message = "we can carry both"
 else:
-   message = "You can't afford both devices"
+    message = "You can't carry both"
 
-# sense.show_message(message, scroll_speed=0.05)
+# sense.show_message(message, scroll_speed=0.05)  
 # what do you expect to see here?
 
-# GO!
+# Start!
 
-# Now change the value of tablet_cost to 260 and run it again
+# Now change the value of nut_weight to 260 and run it again
 # What should the message be this time?
 
-# GO!
+# Start!
 
 # Is this right? You might need to change the comparison operator to >=
 # This means 'greater than or equal to'
 
-raspberry_pi = 25
-pies = 3 * raspberry_pi
+tire_weight = 25
+count = 3	
+tires_weight = count * tire
 
-total_cost = total_cost + pies
+total_weight = total_weight + tires_weight
 
-if total_cost <= money:
-    message = "You have enough money for 3 raspberry pies as well"
+if total_weight <= max_weight:
+    message = "You can carry the tires as well"
 else:
-    message = "You can't afford 3 raspberry pies"
+    message = "You can't carry " + str(count) + "tires as well"
 
-# sense.show_message(message)
+# sense.show_message(message)  
 # what do you expect to see here?
 
-# GO!
+# Start!
 
+
+
+# === NEXT ===
 # 10. You can keep many items in a type of variable called a list
 
 colours = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
@@ -152,11 +176,11 @@ colours = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
 
 # print('Black' in colours)  # Prints True or False
 
-# GO!
+# Start!
 
 # Is it possible to print a bool value on the LEDs?
 
-# GO!
+# Start!
 
 # You can add to the list with append
 
@@ -165,7 +189,7 @@ colours.append('White')
 
 # print('Black' in colours)  # Should this be different now?
 
-# GO!
+# Start!
 
 # You can add a list to a list with extend
 
@@ -175,102 +199,112 @@ colours.extend(more_colours)
 
 # Try printing the list to see what's in it
 
-# GO!
+# Start!
 
+
+
+# === NEXT ===
 # 11. You can use a loop to look over all the items in a list
 
-my_class = ['Sarah', 'Bob', 'Jim', 'Tom', 'Lucy', 'Sophie', 'Liz', 'Ed']
+my_items = ['nut', 'skrew', 'tire', 'shock absorber']
 
 # Below is a multi-line comment
 # Delete the '''from before and after to uncomment the block
 
 '''
-for student in my_class:
-    print(student)
+for item in my_items:
+    print(item)
 '''
 
-# Add two the names of people in your group to this list
+# Add two additional itens to this list
 
 # Remember the difference between append and extend. You can use either.
 
-# Now write a loop to print a number (starting from 1) before each name
+# Now write a loop to print a number (starting from 1) before each item
 
-# GO!
+# Start!
 
+
+
+# === NEXT ===
 # 12. Another useful data structure is a dictionary
 
-# Dictionaries contain key-value pairs like an address book maps name
-# to number
+# Dictionaries contain key-value pairs like the item weight relationships 
 
-addresses = {
-    'Lauren': '0161 5673 890',
-    'Amy': '0115 8901 165',
-    'Daniel': '0114 2290 542',
-    'Emergency': '999'
+weight_items = {
+    'nut': 100,
+    'skrew': 124,
+    'show absorber': 250
 }
 
-# You access dictionary elements by looking them up with the key:
+# You access the elements by looking them up with the key:
 
-# print(addresses['Amy'])
+# print(weight_items['nut'])
 
 # You can check if a key or value exists in a given dictionary:
 
-# print('David' in addresses)  # [False]
-# print('Daniel' in addresses)  # [True]
-# print('999' in addresses)  # [False]
-# print('999' in addresses.values())  # [True]
-# print(999 in addresses.values())  # [False]
+# print('wheel' in weight_items)  # [False]
+# print('nut' in weight_items)  # [True]
+# print('250' in weight_items)  # [False]
+# print(250 in weight_items.values())  # [True]
 
-# GO!
+# Start!
 
-# Note that 999 was entered in to the dictionary as a string, not an integer
-# Think: what would happen if phone numbers were stored as integers?
-# Try changing Amy's phone number to a new number
+# Note that 250 was entered in to the dictionary as a integer
+# Try changing skew weight to a new amount
 
-# addresses['Amy'] = '0115 236 359'
-# print(addresses['Amy'])
+# weight_items['skrew'] = 140
+# print(weight_items['skrew'])
 
-# GO!
+# Start!
 
-# Delete Daniel from the dictinary
+# Delete nut from the dictionary
 
-# print('Daniel' in addresses)  # [True]
-# del addresses['Daniel']
-# print('Daniel' in addresses)  # [False]
+# print('nut' in addresses)  # [True]
+# del weight_items['nut']
+# print('nut' in weight_items)  # [False]
 
-# GO!
+# Start!
 
 # You can also loop over a dictionary and access its contents:
 '''
-for name in addresses:
-  print(name, addresses[name])
+for item in weight_items:
+    print(item, weight_items[item])
 '''
-# GO!
+# Start!
 
-# 13. Sometimes we need a endless loop
+
+
+# === NEXT ===
+# 13. Sometimes we need a endless loop 
 
 # Below is a multi-line comment
 # Delete the '''from before and after to uncomment the block
 
 '''
-while True:
-   print(my_class)
-'''
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+''' 
 
 # What is 'while' doing? How can I stop that?
+# i += 1 is a shortcut for i = i + 1 
 
-# GO!
+# Start!
 
+
+
+# === NEXT ===
 # 14. Additional to internal functions, we build our own functions
 
 # - first, we need a definition part to define the function
 
-# - afterwards we have to excute it
-'''
-def goodwork(person):
-    sense.show_message("Good work, " + person + ".")
+# - afterwards we have to execute it
 
-goodwork('Lars')
-goodwork('Holger')
-'''
-# GO!
+def good_work(bot):
+    sense.show_message("Good work, " + bot + "!")
+
+good_work('Sbot')
+
+# Start!
